51ed6334e33fbd07cec4bf5cde03b5678a409a46
nulltask <nulltask@gmail.com>
2012-07-27 19:16:20 +0900

    typo

--- socket/index.js
+++ socket/index.js
@@ -5,7 +5,7 @@ var inspect = require('util').inspect
   , parseCookie = connect.utils.parseCookie
   , Rail = require('../lib/rail')
   , rail = new Rail()
-  , Neck = require('../lib/nack')
+  , Neck = require('../lib/neck')
   , neck = new Neck(9)
   , Session = connect.middleware.session.Session;
 
